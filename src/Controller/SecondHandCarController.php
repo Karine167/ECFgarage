a370822134fc7531d9ddbc3039b66827e5c91ac5
Rude Karine <rude.karine@gmail.com>
2024-02-12 11:05:33 +0100

    Affichages des photos dans un carroussel

--- src/Controller/SecondHandCarController.php
+++ src/Controller/SecondHandCarController.php
@@ -2,10 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\SecondHandCar;
 use App\Repository\GaleryRepository;
 use App\Repository\InfosRepository;
 use App\Repository\SecondHandCarRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -41,6 +43,35 @@ class SecondHandCarController extends AbstractController
             'imagePath' => $imagePath,
             'photos' => $photos,
             'photoPath' => $photoPath
+        ]);
+    }
+
+    #[Route('/advert/{id}', name: 'app_advert_oneCar')]
+    public function showVehicle(SecondHandCar $secondHandCar, InfosRepository $infosRepository, GaleryRepository $galeryRepository): Response
+    {
+        dump($secondHandCar);
+        //recherche du path du logo
+        $mappingsParams = $this->getParameter('vich_uploader.mappings');
+        $imagePath = $mappingsParams['infos']['uri_prefix'].'/';
+        
+        //recherche des infos de l'accueil, du header et du footer 
+        $infos = $infosRepository->getInfos();
+
+        $id = $secondHandCar->getId();
+        dump($id);
+        // recherche de toutes les images du véhicule
+        $photos = $galeryRepository->findBy(['vehicle'=> $id]);
+        dump($photos);
+        //recherche du path des images 
+        $mappingsParams = $this->getParameter('vich_uploader.mappings');
+        $photoPath = $mappingsParams['galeries']['uri_prefix'].'/'; 
+
+        return $this->render('advert/showOneCar.html.twig', [
+            'infos' => $infos,
+            'imagePath' => $imagePath,
+            'secondHandCar' => $secondHandCar, 
+            'photos' => $photos,
+            'photoPath' => $photoPath 
         ]);
     }
 }
