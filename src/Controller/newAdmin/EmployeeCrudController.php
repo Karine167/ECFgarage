95f8be31987b18975bf563447012a9be682d8ead
Rude Karine <rude.karine@gmail.com>
2024-05-30 12:10:25 +0200

    Ajout de la fonction delete dans le EmployeeCrudController

--- src/Controller/newAdmin/EmployeeCrudController.php
+++ src/Controller/newAdmin/EmployeeCrudController.php
@@ -7,6 +7,7 @@ use App\Form\UserType;
 use App\Repository\InfosRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
@@ -14,11 +15,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[Route('/newAdmin/employee', name: 'admin_employee')]
+use function PHPUnit\Framework\isNull;
+
+#[Route('/newAdmin/employee', name: 'admin_employee_')]
 class EmployeeCrudController extends AbstractController 
 {
 
-    #[Route('/show/{id}', methods:['GET'], name: '_show')]
+    #[Route('/show/{id}', methods:['GET'], name: 'show')]
     public function showEmployee(int $id, User $employee, InfosRepository $infosRepository, UserRepository $userRepository, Security $security): Response
     {
         $page = 'admin/new_dashboard/employee_show.html.twig';
@@ -50,10 +53,10 @@ class EmployeeCrudController extends AbstractController
         };   
     }
 
-    #[Route('/edit/{id}', methods:['GET'], name: '_edit')]
-    #[Route('/create', name: '_create')]
+    #[Route('/edit/{id}', name: 'edit')]
+    #[Route('/create', name: 'create')]
     #[IsGranted('ROLE_ADMIN')]
-    public function edit(Request $request, EntityManagerInterface $em, ?User $user = null, InfosRepository $infosRepository): Response
+    public function edit(Request $request, EntityManagerInterface $em, ?User $employee = null, InfosRepository $infosRepository, Security $security): Response
     {
         $page = 'admin/new_dashboard/employee_edit.html.twig';
         //recherche du path du logo
@@ -62,20 +65,23 @@ class EmployeeCrudController extends AbstractController
         
         //recherche des infos de l'accueil, du header et du footer 
         $infos = $infosRepository->getInfos();
+        $isCreate = !($employee->getId());
 
-        $isCreate = !$user;
-        $employee = $user ?? new User(); 
-        $form = $this->createForm(UserType::class, $employee);
+        $user = $security->getUser();
+
+        $employeeToModify = $employee ?? new User(); 
+        $form = $this->createForm(UserType::class, $employeeToModify);
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            /** @var User $employee */
-            $employee = $form->getData();
-            $em->persist($employee);
+            dump($employeeToModify);
+            /** @var User $employeeToModify */
+            $employeeToModify = $form->getData();
+            $em->persist($employeeToModify);
             $em->flush();
 
-            $this->addFlash('success', $isCreate ? 'L\'employé a bien été créé' : 'L\'employé a été modifié');
-            return $this->redirectToRoute('admin_employee');
+            $this->addFlash('success', $isCreate ? 'L\'employé a bien été créé' : 'L\'employé a bien été modifié');
+            return $this->redirectToRoute('admin_employee_list');
         }
 
         return $this->render('admin/new_dashboard/newDashboard.html.twig', [
@@ -83,11 +89,44 @@ class EmployeeCrudController extends AbstractController
             'infos' => $infos,
             'imagePath' => $imagePath,
             'page' => $page,
-            'employee' => $employee,
+            'employee' => $employeeToModify,
             'form' => $form,
             'is_create' => $isCreate
         ]);   
-        
     }
+        
+    #[Route('/delete/{id}', name: 'delete')]
+    #[IsGranted('ROLE_ADMIN')]
+    public function delete(EntityManagerInterface $em, User $employee, InfosRepository $infosRepository, Security $security): Response
+    {
+        $page = 'admin/new_dashboard/employee_edit.html.twig';
+        //recherche du path du logo
+        $mappingsParams = $this->getParameter('vich_uploader.mappings');
+        $imagePath = $mappingsParams['infos']['uri_prefix'].'/';
+        
+        //recherche des infos de l'accueil, du header et du footer 
+        $infos = $infosRepository->getInfos();
+        
+        $user = $security->getUser();
+        // rechercher de l'employé à supprimer 
+        try {
+        $em->remove($employee);
+        $em->flush();
+        $this->addFlash('success', 'L\'employé a bien été supprimé');
+        return $this->redirectToRoute('admin_employee_list');
+        }catch (Exception $e){
+            $this->addFlash('danger', 'L\'employé n\'a pas pu être supprimé.');
+        return $this->render('admin/new_dashboard/newDashboard.html.twig', [
+                    'user' => $user,
+                    'infos' => $infos,
+                    'imagePath' => $imagePath,
+                    'page' => $page,
+                    'employee' => $employee,
+                ]);   
+        }
+
+    }
+
+
 
 }
